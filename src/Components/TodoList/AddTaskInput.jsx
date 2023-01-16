5567e103ecb4958cc4b1267b700078d8877073dd
MohamadAli Saeidi <1saeidima@gmail.com>
2023-01-16 16:43:59 +0330

    modified:   src/Components/TodoList/AddTaskInput.css modified:   src/Components/TodoList/AddTaskInput.jsx modified:   src/Input.css

--- src/Components/TodoList/AddTaskInput.jsx
+++ src/Components/TodoList/AddTaskInput.jsx
@@ -6,7 +6,7 @@ export default function AddTaskInput()
 	return (
 		<div className='add-task-input-container'>
 			<input className='input add-task-input' type="text" placeholder='Add a Task' />
-			<input className='button' type="button" value={'+'} />
+			<input className='button add-task-button' type="button" value={'+'} />
 		</div>
 	)
 }
